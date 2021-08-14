c363803391ce5547a55cd557b8646fe9b1d3888b
kajaku <kajaku.upgrow@gmail.com>
2021-08-14 11:06:56 +0300

    feat(react-three-fiber): improve mist

--- src/components/pearl3DView/Mist.js
+++ src/components/pearl3DView/Mist.js
@@ -8,20 +8,37 @@ export const Mist = () => {
   const texture = useTexture('/pearl-models/patterns/mist-element.png');
   const groupRef = useRef(null);
   const [vec] = useState(new THREE.Vector3());
+
   const [smokeParticles] = useState(
     Array.from({ length: 33 })
-      .map((_, index) => ({
-        positionX: Math.random() * 0.3 - 0.15,
-        positionY: Math.random() * 0.1 + 0.036,
-        positionZ: -0.13 - index * 0.0135,
-        key: index,
-      }))
+      .reduce((acc, _, index) => {
+        const smoke1 = {
+          positionX: Math.random() * 0.3 - 0.35,
+          positionY: Math.random() * 0.1 + 0.036,
+          positionZ: -0.13 - index * 0.014,
+          key: index + '_1',
+        };
+        const smoke2 = {
+          positionX: Math.random() * 0.3,
+          positionY: Math.random() * 0.1 + 0.036,
+          positionZ: -0.13 - index * 0.0135,
+          key: index + '_2',
+        };
+        acc.push(smoke1, smoke2);
+        return acc;
+      }, [])
   );
-
+  //0x6e85d2
   const [material] = useState(new THREE.MeshLambertMaterial({color: 0x6e85d2, map: texture, transparent: true, opacity: 1 }));
-  const [geometry] = useState(new THREE.PlaneGeometry(0.2, 0.25));
+  const [geometry] = useState(new THREE.PlaneGeometry(0.15, 0.15));
   const { camera } = useThree();
 
+  useEffect(() => {
+    return () => {
+      material.dispose();
+      geometry.dispose();
+    }
+  }, [])
   const smokes = useMemo(() => {
     return smokeParticles.map((particle) => (
       <mesh
@@ -37,17 +54,18 @@ export const Mist = () => {
     if (groupRef.current) {
       camera.getWorldDirection( vec );
       vec.y = 0;
-      vec.add(groupRef.current.position);
+      //vec.add(groupRef.current.position);
       groupRef.current.lookAt(vec);
 
       const delta = clock.getDelta();
-      groupRef.current.children.forEach((mesh, i) => {
-        let k = 1;
-        if (i % 2) {
-          k = -1;
+      groupRef.current.children.forEach((mesh) => {
+        mesh.rotation.z += (delta * 15);
+        const positionX = mesh.position.x;
+        if (positionX < -0.35) {
+          mesh.position.x = 0.35;
+        } else {
+          mesh.position.x -= 0.0003;
         }
-        mesh.rotation.z += (delta * 0.2);
-        mesh.position.x = smokeParticles[i].positionX + Math.sin(clock.elapsedTime * 0.4)* 0.1 * k;
       });
     }
   })
