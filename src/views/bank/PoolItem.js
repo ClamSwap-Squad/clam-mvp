c596115a8ef59d16cbefadbc03a971af1e65d74e
Nikita Kupriyanov <kajaku.upgrow@gmail.com>
2021-09-14 13:37:02 +0300

    fix(bank): fix bank logic

--- src/views/bank/PoolItem.js
+++ src/views/bank/PoolItem.js
@@ -175,16 +175,14 @@ const PoolItem = ({
             <p className="text-gray-500 font-semibold text-xs mb-1 leading-none text-center">APR</p>
 
             <div className="font-bold text-black items-center">
-              {selectedPool ? `${String(selectedPool.apr)}%` : `${String(apr)}%`}
+              {`${String(apr)}%`}
               <InfoTooltip text="Annual Percentage Return - non-compounded rate of return" />
             </div>
           </div>
 
           <div className="text-sm block">
             <p className="text-gray-500 font-semibold text-xs mb-1 leading-none">Deposited</p>
-            <p className="font-bold text-gray-300 text-center">
-              {selectedPool ? selectedPool.userDepositAmountInPool : pool.userDepositAmountInPool}
-            </p>
+            <p className="font-bold text-gray-300 text-center">{pool.userDepositAmountInPool}</p>
           </div>
 
           <div className="text-sm block">
@@ -202,7 +200,7 @@ const PoolItem = ({
                   )}
                   viewBox="0 0 20 20"
                 >
-                  <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
+                  <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10" />
                 </svg>
               </button>
             ) : (
@@ -222,9 +220,7 @@ const PoolItem = ({
               <PoolData
                 depositFee={depositFee}
                 urlForExchange={urlForExchange}
-                tvl={formatEther(
-                  selectedPool ? selectedPool.poolLpTokenBalance : pool.poolLpTokenBalance
-                )}
+                tvl={formatEther(pool.poolLpTokenBalance)}
               />
             </div>
 
