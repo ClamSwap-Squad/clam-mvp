d794437a2a28ddc6127f0aee0b9aef0df08bffa3
luisbaltar12 <luisbaltar12@gmail.com>
2022-01-17 08:39:56 -0300

    fix ENV var configs seamless without renamings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // import { resolve } from 'path'
-import path from 'path'
-import { readdirSync } from 'fs'
+import path from "path";
+import { readdirSync } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
 // const paths = {
 //   "@/*": ["./src/*"]
@@ -20,27 +22,35 @@ import { readdirSync } from 'fs'
 
 const absolutePathAliases = {};
 // Root resources folder
-const srcPath = path.resolve('./src/');
+const srcPath = path.resolve("./src/");
 // Ajust the regex here to include .vue, .js, .jsx, etc.. files from the resources/ folder
-const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map((dirent) => dirent.name.replace(/(\.js){1}(x?)/, ''));
+const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map((dirent) =>
+  dirent.name.replace(/(\.js){1}(x?)/, "")
+);
 
 srcRootContent.forEach((directory) => {
   absolutePathAliases[directory] = path.join(srcPath, directory);
-})
+});
 
 const alias = {
   ...absolutePathAliases,
-  "node_modules": path.resolve(__dirname, "./node_modules/")
-}
+  node_modules: path.resolve(__dirname, "./node_modules/"),
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias
-  },
-  define: {
-    'process.env': {},
-    'global': {},
-  }
-})
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias,
+    },
+    define: {
+      global: {},
+      "process.env.REACT_APP_RPC_URL": `"${process.env.REACT_APP_RPC_URL}"`,
+      "process.env.REACT_APP_GA_TRACKING_NO": `"${process.env.REACT_APP_GA_TRACKING_NO}"`,
+      "process.env.REACT_APP_GTM_TRACKING_NO": `"${process.env.REACT_APP_GTM_TRACKING_NO}"`,
+      "process.env.PUBLIC_URL": `"${process.env.PUBLIC_URL}"`,
+      "process.env.NODE_ENV": `"${mode}"`,
+    },
+  });
+};
